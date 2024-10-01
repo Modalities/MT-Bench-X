19b8e02282e236b74ee2c20812a159c850479654
Alexander Weber <alex.a.weber@gmx.de>
2024-10-01 15:05:02 +0200

    Add cross-lingual figur

--- src/mtbenchx/vizualize_radar.py
+++ src/mtbenchx/vizualize_radar.py
@@ -1,4 +1,5 @@
 import json
+import time
 from pathlib import Path
 
 import numpy as np
@@ -15,6 +16,14 @@ def extract_key_info(key: str):
     return {"language": fields[-3], "category": fields[-2], "turn": fields[-1]}
 
 
+def fix_mathjs_error():
+    file_path = Path("tmp_figure.pdf")
+    fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
+    fig.write_image(str(file_path), format="pdf")
+    time.sleep(2)
+    file_path.unlink()
+
+
 def create_rader_plot(df):
     color_mapping = {
         "EN": ["rgb(238, 130, 238)", "rgb(150, 0, 150)"],
@@ -22,13 +31,14 @@ def create_rader_plot(df):
         "IT": ["rgb(255, 176, 0)", "rgb(200, 120, 0)"],
         "FR": ["rgb(254, 97, 0)", "rgb(200, 30, 0)"],
         "DE": ["rgb(212, 48, 31)", "rgb(150, 30, 20)"],
+        "Avg.": ["rgb(100, 50, 250)", "rgb(0, 0, 200)"],
     }
     radar_plot = px.line_polar(
         df,
         r="score",
         theta="category",
         line_close=True,
-        category_orders={"category": ["Coding", "Extraction", "Humanities", "Math", "Reasoning", "Roleplay", "Stem", "Writing"]},
+        category_orders={"category": df["category"].unique()},
         color="Turn",  # Distinguish between turns
         markers=True,
         color_discrete_sequence=color_mapping[lang],
@@ -41,7 +51,7 @@ def create_rader_plot(df):
         font=dict(
             size=18,
         ),
-        margin=dict(l=0, r=0, t=10, b=60),
+        margin=dict(l=25, r=10, t=10, b=30),
         legend=dict(
             x=1.05,
             y=0.965,
@@ -60,33 +70,9 @@ def create_rader_plot(df):
     return radar_plot
 
 
-def fix_mathjax():
-    import time
-
-    import plotly.express as px
-
-    file_Name = "some_Figure.pdf"
-    fig_Throw_Away = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
-    fig_Throw_Away.write_image(file_Name, format="pdf")
-    time.sleep(0.1)
-
-    # delete the dummy file again
-    path_Cwd = Path.cwd()
-    file_2_Del = path_Cwd / file_Name
-    file_2_Del.unlink(missing_ok=True)
-
-    # ------------------------ end first time ------------------------ #ndError exceptions will be ignored (same behavior as the POSIX rm -f command).
-    file_2_Del.unlink(missing_ok=True)
-
-    # ------------------------ end first time ------------------------ #
-
-    # now run your actual code to generate the plot that you like
-
-
 if __name__ == "__main__":
     import argparse
 
-    fix_mathjax()
     parser = argparse.ArgumentParser()
     parser.add_argument("--file-path", required=True, type=Path, help="")
     args = parser.parse_args()
@@ -100,9 +86,24 @@ if __name__ == "__main__":
     df.language = df.language.str.upper()
     df.rename(columns={"turn": "Turn"}, inplace=True)
 
+    # cross-lingual average
+    cross_lingual_avg = df.groupby(["Turn", "category"]).score.mean().reset_index().copy()
+    cross_lingual_avg["language"] = "Avg."
+    df = pd.concat([df, cross_lingual_avg], ignore_index=True)
+
+    # category average
+    # category_avg = df.groupby(["Turn", "language"]).score.mean().reset_index().copy()
+    # category_avg["category"] = "Avg."
+    # df = pd.concat([df, category_avg], ignore_index=True)
+
     languages = df["language"].unique()
 
+    model_name = args.file_path.stem.rsplit("_", 1)[0]
+    out_path = Path(f"visualization/{model_name}/")
+    out_path.mkdir(exist_ok=True, parents=True)
+    fix_mathjs_error()
     for lang in languages:
         df_filtered = df[df["language"] == lang].copy()
         radar_plot = create_rader_plot(df_filtered)
-        radar_plot.write_image(f"mt_bench_{lang}_radar_plot.pdf")
+        out_file_path = out_path / f"{model_name}_mt_bench_{lang.replace('.', '')}_radar_plot.pdf"
+        radar_plot.write_image(str(out_file_path))
